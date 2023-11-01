3b1693c1373bb24b83e09070631a04bb022c494e
chibali <diogomnuness@hotmail.com>
2023-11-01 19:26:28 +0000

    Added Modifier Mag Calculators for maxhealth and maxmana

---
+++ Source/Aura/Private/AbilitySystem/ModMagCalc/MMC_MaxMana.cpp
@@ -0,0 +1,37 @@
+// Hey yo
+
+
+#include "AbilitySystem/ModMagCalc/MMC_MaxMana.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "Interaction/CombatInterface.h"
+
+UMMC_MaxMana::UMMC_MaxMana()
+{
+	IntelligenceDef.AttributeToCapture = UAuraAttributeSet::GetIntelligenceAttribute();
+	IntelligenceDef.AttributeSource = EGameplayEffectAttributeCaptureSource::Target;
+	IntelligenceDef.bSnapshot = false;
+
+	RelevantAttributesToCapture.Add(IntelligenceDef);
+
+}
+
+float UMMC_MaxMana::CalculateBaseMagnitude_Implementation(const FGameplayEffectSpec& Spec) const
+{
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	FAggregatorEvaluateParameters EvaluationParameters;
+	EvaluationParameters.SourceTags = SourceTags;
+	EvaluationParameters.TargetTags = TargetTags;
+
+	float Intelligence = 0.f;
+
+	GetCapturedAttributeMagnitude(IntelligenceDef, Spec, EvaluationParameters, Intelligence);
+
+	Intelligence = FMath::Max<float>(Intelligence, 0.f);
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(Spec.GetContext().GetSourceObject());
+	const int32 PlayerLevel = CombatInterface->GetPlayerLevel();
+
+	return 60.f + 2 * Intelligence + 5 * PlayerLevel;
+}
