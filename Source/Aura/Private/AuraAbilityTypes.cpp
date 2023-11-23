44b67affe144b6a627ed895b9096abd686042b93
chibali <diogomnuness@hotmail.com>
2023-11-23 16:59:04 +0000

    Created subclass for GameplayEffectContext to override getting certain properties derived from the attributeset (e.g. Critical Hits and Blocked Hits)

---
+++ Source/Aura/Private/AuraAbilityTypes.cpp
@@ -0,0 +1,6 @@
+#include "AuraAbilityTypes.h"
+
+bool FAuraGameplayEffectContext::NetSerialize(FArchive& Ar, UPackageMap* Map, bool& bOutSuccess)
+{
+	return true; // TO DO Specify how the struct is gonna be serialized
+}
