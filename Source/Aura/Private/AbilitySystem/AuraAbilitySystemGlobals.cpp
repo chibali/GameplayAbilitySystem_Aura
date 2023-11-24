d1a90963a6f88ddd81b06d105bacdfa558c439d9
chibali <diogomnuness@hotmail.com>
2023-11-24 16:07:08 +0000

    Created AuraAbilitySystemGlobals to initiliaze the AuraGameplayEffectContext

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,10 @@
+// Hey yo
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
