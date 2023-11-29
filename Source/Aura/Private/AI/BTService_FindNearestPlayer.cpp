5d8ed82de811df8035c80efc58cdd59a538472a0
chibali <diogomnuness@hotmail.com>
2023-11-29 17:05:07 +0000

    Added BT Service for finding nearest player through Actor Tags

---
+++ Source/Aura/Private/AI/BTService_FindNearestPlayer.cpp
@@ -0,0 +1,12 @@
+// Hey yo
+
+
+#include "AI/BTService_FindNearestPlayer.h"
+#include "AIController.h"
+
+void UBTService_FindNearestPlayer::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	APawn* OwningPawn = AIOwner->GetPawn();
+}
