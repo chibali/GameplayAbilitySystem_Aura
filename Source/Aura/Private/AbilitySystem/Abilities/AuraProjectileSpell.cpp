12eb8538880d1b27430dd4478cb0001aaa1839cd
chibali <diogomnuness@hotmail.com>
2023-12-13 13:44:41 +0000

    Finished polishing the enemy Shaman, now summons minions correctly according to the minion count on field, else it will fire a projectile. Fixed some optimization bugs

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -9,16 +9,20 @@
 #include "AbilitySystemComponent.h"
 #include "Aura/Public/AuraGameplayTags.h"
 
-void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectleTargetLocation, const FGameplayTag& CombatSocketTag)
+void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectleTargetLocation, const FGameplayTag& CombatSocketTag, bool bOverridePitch, float PitchOverride)
 {
 	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), CombatSocketTag);
+
 	FRotator Rotation = (ProjectleTargetLocation - SocketLocation).Rotation();
+	if (bOverridePitch)
+	{
+		Rotation.Pitch = PitchOverride;
+	}
 
 	FTransform SpawnTransform;
 	SpawnTransform.SetLocation(SocketLocation);
-
 	SpawnTransform.SetRotation(Rotation.Quaternion());
 
 	AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
