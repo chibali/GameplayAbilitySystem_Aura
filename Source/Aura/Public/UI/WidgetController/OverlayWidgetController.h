8d59f73bda68760bd910e0f4d2549227fd5160dc
chibali <diogomnuness@hotmail.com>
2023-12-18 17:15:28 +0000

    Created a DataAsset for storing abilities and displaying them in the HUD. Created a custom Log channel

--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -8,6 +8,7 @@
 #include "OverlayWidgetController.generated.h"
 
 class UAuraUserWidget;
+class UAbilityInfo;
 struct FOnAttributeChangeData;
 
 USTRUCT(BlueprintType)
@@ -61,6 +62,9 @@ public:
 protected:
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Widget Data")
 	TObjectPtr<UDataTable> MessageWidgetDataTable;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget Data")
+	TObjectPtr<UAbilityInfo> AbilityInfo;
 
 	template<typename T>
 	T* GetDataTableRowByTag(UDataTable* DataTable, const FGameplayTag& Tag);
