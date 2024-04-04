351606bcb31e868a8e98bf200f09c7642f6423d0
chibali <diogomnuness@hotmail.com>
2024-04-04 15:09:11 +0100

    Added a Checkpoint class.

---
+++ Source/Aura/Private/Checkpoint/AuraCheckpoint.cpp
@@ -0,0 +1,43 @@
+// Hey yo
+
+
+#include "Checkpoint/AuraCheckpoint.h"
+#include "Components/SphereComponent.h"
+
+AAuraCheckpoint::AAuraCheckpoint(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	CheckpointMesh = CreateDefaultSubobject<UStaticMeshComponent>("CheckpointMesh");
+	CheckpointMesh->SetupAttachment(GetRootComponent());
+	CheckpointMesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	CheckpointMesh->SetCollisionResponseToAllChannels(ECR_Block);
+
+	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
+	Sphere->SetupAttachment(CheckpointMesh);
+	Sphere->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+}
+
+void AAuraCheckpoint::OnSphereOverlap(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor->ActorHasTag(FName("Player")))
+	{
+		HandleGlowEffects();
+	}
+}
+
+void AAuraCheckpoint::BeginPlay()
+{
+	Super::BeginPlay();
+	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AAuraCheckpoint::OnSphereOverlap);
+}
+
+void AAuraCheckpoint::HandleGlowEffects()
+{
+	Sphere->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	UMaterialInstanceDynamic* DynamicMaterialInstance = UMaterialInstanceDynamic::Create(CheckpointMesh->GetMaterial(0), this);
+	CheckpointMesh->SetMaterial(0, DynamicMaterialInstance);
+	CheckpointReached(DynamicMaterialInstance);
+}
---
+++ Source/Aura/Public/Checkpoint/AuraCheckpoint.h
@@ -0,0 +1,39 @@
+// Hey yo
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerStart.h"
+#include "AuraCheckpoint.generated.h"
+
+class USphereComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAuraCheckpoint : public APlayerStart
+{
+	GENERATED_BODY()
+	
+public:
+	AAuraCheckpoint(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	UFUNCTION()
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlapComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void CheckpointReached(UMaterialInstanceDynamic* DynamicMaterialInstance);
+
+	void HandleGlowEffects();
+
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> CheckpointMesh;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> Sphere;
+};
